d3a576c43b868dacce6fb12d103ca5f515b23641
Greeny32 <iepicgreen@gmail.com>
2026-06-19 11:27:18 +0100

    call api backend

---
+++ scripts/api.js
@@ -0,0 +1,22 @@
+// SCRIPT TO CALL API AND STORE FIXTURES IN JSON FILE
+const fs = require("fs");
+
+async function update(){
+    // Call api
+    const response = await fetch(
+        "https://api.football-data.org/v4/competitions/WC/matches",
+        {
+            headers: {
+                "X-AUTH-TOKEN": process.env.FTBL_KEY
+            }
+        }
+    );
+    const data = await response.json();
+
+    fs.writeFileSync(
+        "data/fixtures.json",
+        JSON.stringify(data, null, 2)
+    );
+}
+
+update();
