d5c0903bd138546aa6014086a175661520d58932
Greeny32 <iepicgreen@gmail.com>
2026-06-19 11:49:28 +0100

    api

--- scripts/api.js
+++ scripts/api.js
@@ -1,6 +1,9 @@
 // SCRIPT TO CALL API AND STORE FIXTURES IN JSON FILE
 const fs = require("fs");
 
+console.log("KEY EXISTS:", !!process.env.FTBL_KEY);
+console.log("KEY VALUE:", process.env.FTBL_KEY);
+
 async function update(){
     // Call api
     const response = await fetch(
